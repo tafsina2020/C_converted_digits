d5241b692dadb089e8035a8a570c95bbf418932e
tafsina2020 <59949514+tafsina2020@users.noreply.github.com>
2022-10-17 05:48:35 +0600

    C programming_convert decimal to octa & hexa

---
+++ converted_files.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    int num1, num2, num3;
+    printf("Enter a Decimal number: ");
+    scanf("%d", &num1);
+
+    printf("The Octal number is: %o\n", &num2);
+    printf("The Hexa-Decimal number is: %x", &num3);
+
+    return 0;
+}
